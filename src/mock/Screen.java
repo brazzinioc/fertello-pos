348fbb5484afa07de069ebd336179ca811f57d65
Eric Mogollon <eric@marteu.com>
2024-04-19 20:39:15 -0500

    Add Screen class with welcome and product selection menus

---
+++ src/mock/Screen.java
@@ -0,0 +1,82 @@
+package mock;
+
+public class Screen {
+    public static void main(String[] args) {
+        // Start Menu de bienvenida
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Bienvenido a CUEROS FERTELLO S.A.C.");
+        System.out.println("------------------------------------------------------------\n");
+        System.out.println("Seleccione su rol de usuario\n");
+        System.out.println("A: Administración");
+        System.out.println("V: Ventas");
+        System.out.println("P: Producción\n");
+        System.out.println("------------------------------------------------------------\n");
+        // End Menu de bienvenida
+
+        // Start Menu de seleccion de productos
+        System.out.println("Seleccione el número del producto a fabricar\n");
+        System.out.println("------------------------------------------------------------\n");
+        System.out.println(
+                "1: Producto SKU001\nColor: Negro\nModelo: Clásica\nColección: Colección Elegance\nGénero: Unisex\nTamaño: Grande");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "2: Producto SKU002\nColor: Marrón\nModelo: Con broche\nColección: Colección Vintage\nGénero: Masculino\nTamaño: Mediano");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "3: Producto SKU003\nColor: Azul\nModelo: Bifold\nColección: Colección Moderna\nGénero: Femenino\nTamaño: Pequeño");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "4: Producto SKU004\nColor: Rojo\nModelo: Minimalista\nColección: Colección Urban\nGénero: Unisex\nTamaño: Pequeño");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "5: Producto SKU005\nColor: Verde\nModelo: Con cierre\nColección: Colección Adventure\nGénero: Masculino\nTamaño: Mediano");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "6: Producto SKU006\nColor: Gris\nModelo: Trifold\nColección: Colección Classic\nGénero: Femenino\nTamaño: Grande");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "7: Producto SKU007\nColor: Café\nModelo: Con grabado\nColección: Colección Vintage\nGénero: Unisex\nTamaño: Mediano");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "8: Producto SKU008\nColor: Azul Marino\nModelo: Minimalista\nColección: Colección Moderna\nGénero: Masculino\nTamaño: Pequeño");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "9: Producto SKU009\nColor: Negro\nModelo: Con monedero\nColección: Colección Elegance\nGénero: Femenino\nTamaño: Grande");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "10: Producto SKU010\nColor: Blanco\nModelo: Bifold\nColección: Colección Urban\nGénero: Unisex\nTamaño: Mediano");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "11: Producto SKU011\nColor: Marrón Oscuro\nModelo: Clásica\nColección: Colección Adventure\nGénero: Masculino\nTamaño: Grande");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "12: Producto SKU012\nColor: Rosado\nModelo: Con cierre\nColección: Colección Moderna\nGénero: Femenino\nTamaño: Pequeño");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "13: Producto SKU013\nColor: Azul Claro\nModelo: Trifold\nColección: Colección Classic\nGénero: Unisex\nTamaño: Mediano");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "14: Producto SKU014\nColor: Gris Oscuro\nModelo: Con broche\nColección: Colección Vintage\nGénero: Masculino\nTamaño: Grande");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "15: Producto SKU015\nColor: Rojo Intenso\nModelo: Minimalista\nColección: Colección Urban\nGénero: Femenino\nTamaño: Pequeño");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "16: Producto SKU016\nColor: Verde Militar\nModelo: Con monedero\nColección: Colección Elegance\nGénero: Unisex\nTamaño: Grande");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "17: Producto SKU017\nColor: Café Claro\nModelo: Bifold\nColección: Colección Adventure\nGénero: Masculino\nTamaño: Mediano");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "18: Producto SKU018\nColor: Negro Mate\nModelo: Clásica\nColección: Colección Moderna\nGénero: Femenino\nTamaño: Pequeño");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "19: Producto SKU019\nColor: Blanco Perla\nModelo: Con cierre\nColección: Colección Classic\nGénero: Unisex\nTamaño: Grande");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(
+                "20: Producto SKU020\nColor: Marrón Chocolate\nModelo: Trifold\nColección: Colección Vintage\nGénero: Masculino\nTamaño: Mediano");
+        System.out.println("------------------------------------------------------------");
+        // End Menu de seleccion de productos
+    }
+
+}
