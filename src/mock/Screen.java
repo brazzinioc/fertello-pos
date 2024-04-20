91b0d8e39da2b1caed75680221df3fc322f8a9da
Eric Mogollon <eric@marteu.com>
2024-04-19 21:28:07 -0500

    Update Screen class with additional functionality

--- src/mock/Screen.java
+++ src/mock/Screen.java
@@ -2,19 +2,8 @@ package mock;
 
 public class Screen {
     public static void main(String[] args) {
-        // Start Menu de bienvenida
-        System.out.println("------------------------------------------------------------");
-        System.out.println("Bienvenido a CUEROS FERTELLO S.A.C.");
-        System.out.println("------------------------------------------------------------\n");
-        System.out.println("Seleccione su rol de usuario\n");
-        System.out.println("A: Administración");
-        System.out.println("V: Ventas");
-        System.out.println("P: Producción\n");
-        System.out.println("------------------------------------------------------------\n");
-        // End Menu de bienvenida
-
         // Start Menu de seleccion de productos
-        System.out.println("Seleccione el número del producto a fabricar\n");
+        System.out.println("Ingrese el número del producto a fabricar o ingrese S para salir:\n");
         System.out.println("------------------------------------------------------------\n");
         System.out.println(
                 "1: Producto SKU001\nColor: Negro\nModelo: Clásica\nColección: Colección Elegance\nGénero: Unisex\nTamaño: Grande");
@@ -36,47 +25,52 @@ public class Screen {
         System.out.println("------------------------------------------------------------");
         System.out.println(
                 "7: Producto SKU007\nColor: Café\nModelo: Con grabado\nColección: Colección Vintage\nGénero: Unisex\nTamaño: Mediano");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "8: Producto SKU008\nColor: Azul Marino\nModelo: Minimalista\nColección: Colección Moderna\nGénero: Masculino\nTamaño: Pequeño");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "9: Producto SKU009\nColor: Negro\nModelo: Con monedero\nColección: Colección Elegance\nGénero: Femenino\nTamaño: Grande");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "10: Producto SKU010\nColor: Blanco\nModelo: Bifold\nColección: Colección Urban\nGénero: Unisex\nTamaño: Mediano");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "11: Producto SKU011\nColor: Marrón Oscuro\nModelo: Clásica\nColección: Colección Adventure\nGénero: Masculino\nTamaño: Grande");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "12: Producto SKU012\nColor: Rosado\nModelo: Con cierre\nColección: Colección Moderna\nGénero: Femenino\nTamaño: Pequeño");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "13: Producto SKU013\nColor: Azul Claro\nModelo: Trifold\nColección: Colección Classic\nGénero: Unisex\nTamaño: Mediano");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "14: Producto SKU014\nColor: Gris Oscuro\nModelo: Con broche\nColección: Colección Vintage\nGénero: Masculino\nTamaño: Grande");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "15: Producto SKU015\nColor: Rojo Intenso\nModelo: Minimalista\nColección: Colección Urban\nGénero: Femenino\nTamaño: Pequeño");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "16: Producto SKU016\nColor: Verde Militar\nModelo: Con monedero\nColección: Colección Elegance\nGénero: Unisex\nTamaño: Grande");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "17: Producto SKU017\nColor: Café Claro\nModelo: Bifold\nColección: Colección Adventure\nGénero: Masculino\nTamaño: Mediano");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "18: Producto SKU018\nColor: Negro Mate\nModelo: Clásica\nColección: Colección Moderna\nGénero: Femenino\nTamaño: Pequeño");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "19: Producto SKU019\nColor: Blanco Perla\nModelo: Con cierre\nColección: Colección Classic\nGénero: Unisex\nTamaño: Grande");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(
-                "20: Producto SKU020\nColor: Marrón Chocolate\nModelo: Trifold\nColección: Colección Vintage\nGénero: Masculino\nTamaño: Mediano");
-        System.out.println("------------------------------------------------------------");
         // End Menu de seleccion de productos
+
+        // Start Menu de bienvenida
+        System.out.println("------------------------------------------------------------\n");
+        System.out.println("Bienvenido a CUEROS FERTELLO S.A.C.\n");
+        System.out.println("------------------------------------------------------------\n");
+        System.out.println("Seleccione su rol de usuario: Ingrese A, V o P\n");
+        System.out.println("A: Administración");
+        System.out.println("V: Ventas");
+        System.out.println("P: Producción");
+        System.out.println("S: Salir\n");
+        System.out.println("------------------------------------------------------------\n");
+        // End Menu de bienvenida
+
+        // Start Menu de Administración
+        System.out.println("Administración / Inicio\n");
+        System.out.println("------------------------------------------------------------\n");
+        System.out.println("Seleccione el modulo a consultar:\n");
+        System.out.println("I: Inventario");
+        System.out.println("V: Reporte de Ventas");
+        System.out.println("P: Pedidos");
+        System.out.println("F: Producción");
+        System.out.println("S: Salir\n");
+        System.out.println("------------------------------------------------------------\n");
+        // End Menu de Administración
+
+        // Start Menu de Ventas
+        System.out.println("Ventas / Inicio\n");
+        System.out.println("------------------------------------------------------------\n");
+        System.out.println("Seleccione el modulo a consultar:\n");
+        System.out.println("R: Registrar nueva venta");
+        System.out.println("C: Consulta de Ventas");
+        System.out.println("T: Ventas totales");
+        System.out.println("S: Salir\n");
+        System.out.println("------------------------------------------------------------\n");
+        // End Menu de Ventas
+
+        // Start Menu de Producción
+        System.out.println("Producción / Inicio\n");
+        System.out.println("------------------------------------------------------------\n");
+        System.out.println("Seleccione el modulo a consultar:\n");
+        System.out.println("P: Producir nuevo producto");
+        System.out.println("C: Consulta de producto fabricados");
+        System.out.println("S: Salir\n");
+        System.out.println("------------------------------------------------------------\n");
+        // End Menu de Producción
     }
 
 }
