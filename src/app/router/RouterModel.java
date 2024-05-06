3f35295907ba2cc47b3dc8480c4676f6b149d270
Eric Mogollon <eric@marteu.com>
2024-05-05 23:39:26 -0500

    Refactor RouterController and RouterModel to remove unused imports and update module names

--- src/app/router/RouterModel.java
+++ src/app/router/RouterModel.java
@@ -5,7 +5,6 @@ import app.utils.Constants;
 
 public class RouterModel {
     private String moduleName = Constants.APP_MENU;
-    private String[] moduleItems = { Constants.ADMIN_MODULE, Constants.MANUFACTURE_MODULE, Constants.SALES_MODULE };
 
     public String mainModuleName() {
         return moduleName;
@@ -13,21 +12,14 @@ public class RouterModel {
 
     public String[] mainModuleItems(Roles userRole) {
         switch (userRole) {
-            case Roles.ADMIN:
+            case ADMIN:
                 return new String[] { Constants.ADMIN_MODULE };
-            case Roles.PRODUCTION:
+            case PRODUCTION:
                 return new String[] { Constants.MANUFACTURE_MODULE };
-            case Roles.SALES:
+            case SALES:
                 return new String[] { Constants.SALES_MODULE };
             default:
                 return new String[] {};
-        }
-    }
-
-    public void showMainMenu() {
-        System.out.println(moduleName);
-        for (int i = 0; i < moduleItems.length; i++) {
-            System.out.println((i + 1) + ". " + moduleItems[i]);
         }
     }
 }
